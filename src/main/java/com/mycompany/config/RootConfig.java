3d0d5fec55a5d0d3dc6cf20b2bac58acf76909b4
Akmal <akmal9433@gmail.com>
2020-01-22 23:31:04 +0300

    Add configurations for web

---
+++ src/main/java/com/mycompany/config/RootConfig.java
@@ -0,0 +1,13 @@
+package com.mycompany.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(value = "com.mycompany",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class))
+public class RootConfig {
+
+}
