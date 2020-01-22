8b67a683194f963543d119439cc2fb8806128118
Akmal <akmal9433@gmail.com>
2020-01-23 00:27:50 +0300

    Add WebSecurityConfig to WebAppConfig

--- src/main/java/com/mycompany/config/WebAppConfig.java
+++ src/main/java/com/mycompany/config/WebAppConfig.java
@@ -3,6 +3,7 @@ package com.mycompany.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +12,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @ComponentScan(value = "com.mycompany.controller",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class))
+@Import(
+        {
+                WebSecurityConfig.class
+        }
+)
 public class WebAppConfig implements WebMvcConfigurer {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
