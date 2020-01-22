d1d49429d359c3e5db18e3ca2dca1da0f8c946e6
Akmal <akmal9433@gmail.com>
2020-01-22 23:58:46 +0300

    Add mocked methods to controllers

--- src/main/java/com/mycompany/controller/AnswerController.java
+++ src/main/java/com/mycompany/controller/AnswerController.java
@@ -1,10 +1,36 @@
 package com.mycompany.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mycompany.controller.dto.Answer;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/answers")
 public class AnswerController {
+    @GetMapping(value = "/{questionId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public List<Answer> getAnswers(@PathVariable Long questionId) {
+        return Collections.EMPTY_LIST;
+    }
+
+    @PostMapping(value = "/{questionId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Answer addAnswer(@PathVariable Long questionId,
+                            @RequestBody Answer answer) {
+        return answer;
+    }
+
+    @PutMapping(value = "/{answerId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Answer putAnswer(@PathVariable Long answerId,
+                            @RequestBody Answer answer) {
+        return answer;
+    }
+
+    @DeleteMapping(value = "/{answerId}")
+    public Answer deleteAnswer(@PathVariable Long answerId) {
+        return new Answer();
+    }
+
 
 }
--- src/main/java/com/mycompany/controller/QuestionController.java
+++ src/main/java/com/mycompany/controller/QuestionController.java
@@ -1,10 +1,33 @@
 package com.mycompany.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mycompany.controller.dto.Question;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collections;
+import java.util.List;
 
 @RestController
 @RequestMapping("/questions")
 public class QuestionController {
+    @GetMapping
+    public List<Question> questions() {
+        return Collections.EMPTY_LIST;
+    }
 
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Question addQuestion(@RequestBody Question question) {
+        return question;
+    }
+
+    @PutMapping(value = "/{questionId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Question putQuestion(@PathVariable Long questionId,
+                                @RequestBody Question question) {
+        return question;
+    }
+
+    @DeleteMapping(value = "/{questionId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Question deleteQuestion(@PathVariable Long questionId) {
+        return new Question();
+    }
 }
